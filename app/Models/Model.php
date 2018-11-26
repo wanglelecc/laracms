78621d0fc049933297cce7c3360e4c648f63e6c6
wanglelecc <wanglelecc@gmail.com>
2018-11-26 10:43:15 +0800

    修复安装时找不到 laracms-generator 包问题

---
+++ app/Models/Model.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * LaraCMS - CMS based on laravel
+ *
+ * @category  LaraCMS
+ * @package   Laravel
+ * @author    Wanglelecc <wanglelecc@gmail.com>
+ * @date      2018/06/06 09:08:00
+ * @copyright Copyright 2018 LaraCMS
+ * @license   https://opensource.org/licenses/MIT
+ * @github    https://github.com/wanglelecc/laracms
+ * @link      https://www.laracms.cn
+ * @version   Release 1.0
+ */
+
+namespace App\Models;
+
+class Model extends Wanglelecc\Laracms\Models\Model
+{
+
+}
